6746f055f354fb424eb1e704e0e70cbc02c1a8e1
j-jackson9 <jordancjack.20@gmail.com>
2025-01-04 15:09:16 -0600

    -started to intergrate api

---
+++ index.js
@@ -0,0 +1,10 @@
+function getWeather() {
+    const apiKey = '963dbb87d5f4a10699f2e318c226910f';
+    const weatherInput = document.getElementsByClassName('weather-input').value;
+    if (!weatherInput) {
+        alert('Please enter a city name');
+        return;
+    }
+
+    
+}
